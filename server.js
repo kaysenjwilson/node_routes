905b5488ca864e6ef96a96a8aa8fed74f3475c51
kaysenjwilson <kwilson26@s.tooeletech.edu>
2023-11-01 10:41:29 -0600

    first commit

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+const mongodb = require("./db/connect");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(cors()).use("/", require("./routes"));
+
+mongodb.initDb((err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    app.listen(PORT);
+    console.log(
+      "\x1b[34m%s\x1b[0m",
+      `Connected to DB and listening on ${PORT}`
+    );
+  }
+});
